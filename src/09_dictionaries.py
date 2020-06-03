657a13dcc45d885dea4dfe420c522d0db9745b64
David Mizativan-Ortega <dortega5185@gmail.com>
2020-06-03 18:27:11 -0500

    mvp

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -16,25 +16,14 @@ has the following keys:
 """
 
 waypoints = [
-    {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    },
-    {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    },
-    {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
-    }
+    {"lat": 43, "lon": -121, "name": "a place"},
+    {"lat": 41, "lon": -123, "name": "another place"},
+    {"lat": 43, "lon": -122, "name": "a third place"},
 ]
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 45, "lon": -125, "name": "a different place"})
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +31,17 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0] = {"lat": 43, "lon": -130, "name": "not a real place"}
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in range(len(waypoints)):
+    print(waypoints[i])
+
+# class area:
+#     def __init__(self, volume, height):
+#         # self.volume = volume
+#         # self.height = height
+#         super.__init__(volume, height) = self
+
+# super for children/inherited
